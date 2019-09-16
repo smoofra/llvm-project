8433c023153d6ebfea6389df3c62c4c44d833027
Lawrence D'Anna <lawrence_danna@apple.com>
2019-09-16 15:46:29 -0700

    Add tests for SBFile

--- lldb/packages/Python/lldbsuite/test/python_api/file_handle/TestFileHandle.py
+++ lldb/packages/Python/lldbsuite/test/python_api/file_handle/TestFileHandle.py
@@ -1,5 +1,5 @@
 """
-Test lldb Python API for file handles.
+Test lldb lldb Python API for file handles.
 """
 
 from __future__ import print_function
@@ -15,6 +15,22 @@ from lldbsuite.test.decorators import *
 from lldbsuite.test.lldbtest import *
 from lldbsuite.test import lldbutil
 
+
+class BadIO(io.TextIOBase):
+    def writable(self):
+        return True
+    def write(self, s):
+        raise Exception('OH NOE')
+
+
+@contextlib.contextmanager
+def replace_stdout(new):
+    old = sys.stdout
+    sys.stdout = new
+    try:
+        yield
+    finally:
+        sys.stdout = old
 
 def readStrippedLines(f):
     def i():
@@ -34,15 +50,23 @@ def handle_command(debugger, cmd, raise_on_fail=True, collect_result=True):
     else:
         debugger.HandleCommand(cmd)
 
+    debugger.GetOutputFile().Flush()
+    debugger.GetErrorFile().Flush()
+
     if collect_result and raise_on_fail and not ret.Succeeded():
         raise Exception
 
     return ret.GetOutput()
 
 
+
 class FileHandleTestCase(TestBase):
 
     mydir = TestBase.compute_mydir(__file__)
+
+    def comment(self, *args):
+        if self.session is not None:
+            print(*args, file=self.session)
 
     @add_test_categories(['pyapi'])
     @no_debug_info_test
@@ -71,22 +95,6 @@ class FileHandleTestCase(TestBase):
             lldb.SBDebugger.Destroy(debugger)
             with open('output', 'r') as f:
                 self.assertTrue(re.search("is not a valid command", f.read()))
-        finally:
-            self.RemoveTempFile('output')
-            lldb.SBDebugger.Destroy(debugger)
-
-
-    @add_test_categories(['pyapi'])
-    @no_debug_info_test
-    def test_legacy_file_out(self):
-        debugger = lldb.SBDebugger.Create()
-        try:
-            with open('output', 'w') as f:
-                debugger.SetOutputFileHandle(f, False)
-                debugger.GetOutputFileHandle().write('FOO\n')
-                handle_command(debugger, 'script 1+1')
-            with open('output', 'r') as f:
-                self.assertEqual(readStrippedLines(f), ['FOO', '2'])
         finally:
             self.RemoveTempFile('output')
             lldb.SBDebugger.Destroy(debugger)
@@ -126,8 +134,532 @@ class FileHandleTestCase(TestBase):
         finally:
             self.RemoveTempFile('output')
 
+    @add_test_categories(['pyapi'])
+    @no_debug_info_test
+    def test_fileno_out(self):
+
+        debugger = lldb.SBDebugger.Create()
+        try:
+            with open('output', 'w') as f:
+                sbf = lldb.SBFile()
+                sbf.SetDescriptor(f.fileno(), "w", False)
+                status = debugger.SetOutputFile(sbf)
+                if status.Fail():
+                    raise Exception(status)
+                handle_command(debugger, 'script 1+2')
+
+            with open('output', 'r') as f:
+                self.assertEqual(f.read().strip(), '3')
+
+        finally:
+            self.RemoveTempFile('output')
+            lldb.SBDebugger.Destroy(debugger)
+
+    @add_test_categories(['pyapi'])
+    @no_debug_info_test
+    def test_file_out(self):
+
+        debugger = lldb.SBDebugger.Create()
+        try:
+            with open('output', 'w') as f:
+                status = debugger.SetOutputFile(f)
+                if status.Fail():
+                    raise Exception(status)
+                handle_command(debugger, 'script 1+2')
+
+            with open('output', 'r') as f:
+                self.assertEqual(f.read().strip(), '3')
+
+        finally:
+            self.RemoveTempFile('output')
+            lldb.SBDebugger.Destroy(debugger)
+
+    @add_test_categories(['pyapi'])
+    @no_debug_info_test
+    def test_fileno_help(self):
+
+        debugger = lldb.SBDebugger.Create()
+        try:
+            with open('output', 'w') as f:
+                sbf = lldb.SBFile()
+                sbf.SetDescriptor(f.fileno(), "w", False)
+                status = debugger.SetOutputFile(sbf)
+                if status.Fail():
+                    raise Exception(status)
+                handle_command(debugger, "help help", collect_result=False)
+
+            with open('output', 'r') as f:
+                self.assertTrue(re.search(r'Show a list of all debugger commands', f.read()))
+
+        finally:
+            self.RemoveTempFile('output')
+            lldb.SBDebugger.Destroy(debugger)
+
+    @add_test_categories(['pyapi'])
+    @no_debug_info_test
+    def test_help(self):
+
+        debugger = lldb.SBDebugger.Create()
+        try:
+            with open('output', 'w') as f:
+                status = debugger.SetOutputFile(f)
+                if status.Fail():
+                    raise Exception(status)
+                handle_command(debugger, "help help", collect_result=False)
+
+            with open('output', 'r') as f:
+                self.assertTrue(re.search(r'Show a list of all debugger commands', f.read()))
+
+        finally:
+            self.RemoveTempFile('output')
+            lldb.SBDebugger.Destroy(debugger)
+
+
+    @add_test_categories(['pyapi'])
+    @no_debug_info_test
+    def test_immediate(self):
+
+        debugger = lldb.SBDebugger.Create()
+        try:
+            with open('output', 'w') as f:
+                ret = lldb.SBCommandReturnObject()
+                ret.SetImmediateOutputFile(f)
+                interpreter = debugger.GetCommandInterpreter()
+                interpreter.HandleCommand("help help", ret)
+                # make sure the file wasn't closed early.
+                f.write("\nQUUX\n")
+
+            with open('output', 'r') as f:
+                output = f.read()
+                self.assertTrue(re.search(r'Show a list of all debugger commands', output))
+                self.assertTrue(re.search(r'QUUX', output))
+
+        finally:
+            self.RemoveTempFile('output')
+            lldb.SBDebugger.Destroy(debugger)
+
+
+    @add_test_categories(['pyapi'])
+    @no_debug_info_test
+    def test_close(self):
+
+        debugger = lldb.SBDebugger.Create()
+        try:
+            with open('output', 'w') as f:
+                status = debugger.SetOutputFile(f)
+                if status.Fail():
+                    raise Exception(status)
+                handle_command(debugger, "help help", collect_result=False)
+                # make sure the file wasn't closed early.
+                f.write("\nZAP\n")
+                lldb.SBDebugger.Destroy(debugger)
+                # check that output file was closed when debugger was destroyed.
+                with self.assertRaises(ValueError):
+                    f.write("\nQUUX\n")
+
+            with open('output', 'r') as f:
+                output = f.read()
+                self.assertTrue(re.search(r'Show a list of all debugger commands', output))
+                self.assertTrue(re.search(r'ZAP', output))
+
+        finally:
+            self.RemoveTempFile('output')
+            lldb.SBDebugger.Destroy(debugger)
+
+
+    @add_test_categories(['pyapi'])
+    @no_debug_info_test
+    def test_fileno_inout(self):
+
+        debugger = lldb.SBDebugger.Create()
+        try:
+            with open('input', 'w') as f:
+                f.write("help help\n")
+
+            with open('output', 'w') as outf, open('input', 'r') as inf:
+
+                outsbf = lldb.SBFile()
+                outsbf.SetDescriptor(outf.fileno(), "w", False)
+                status = debugger.SetOutputFile(outsbf)
+                if status.Fail():
+                    raise Exception(status)
+
+                insbf = lldb.SBFile()
+                insbf.SetDescriptor(inf.fileno(), "r", False)
+                status = debugger.SetInputFile(insbf)
+                if status.Fail():
+                    raise Exception(status)
+
+                opts = lldb.SBCommandInterpreterRunOptions()
+                debugger.RunCommandInterpreter(True, False, opts, 0, False, False)
+                debugger.GetOutputFile().Flush()
+
+            with open('output', 'r') as f:
+                self.assertTrue(re.search(r'Show a list of all debugger commands', f.read()))
+
+        finally:
+            self.RemoveTempFile('output')
+            self.RemoveTempFile('input')
+            lldb.SBDebugger.Destroy(debugger)
+
+
+    @add_test_categories(['pyapi'])
+    @no_debug_info_test
+    def test_inout(self):
+
+        debugger = lldb.SBDebugger.Create()
+        try:
+            with open('input', 'w') as f:
+                f.write("help help\n")
+
+            with open('output', 'w') as outf, open('input', 'r') as inf:
+
+                status = debugger.SetOutputFile(outf)
+                if status.Fail():
+                    raise Exception(status)
+
+                status = debugger.SetInputFile(inf)
+                if status.Fail():
+                    raise Exception(status)
+
+                opts = lldb.SBCommandInterpreterRunOptions()
+                debugger.RunCommandInterpreter(True, False, opts, 0, False, False)
+                debugger.GetOutputFile().Flush()
+
+            with open('output', 'r') as f:
+                self.assertTrue(re.search(r'Show a list of all debugger commands', f.read()))
+
+        finally:
+            self.RemoveTempFile('output')
+            self.RemoveTempFile('input')
+            lldb.SBDebugger.Destroy(debugger)
+
+    @add_test_categories(['pyapi'])
+    @no_debug_info_test
+    def test_binary_inout(self):
+
+        debugger = lldb.SBDebugger.Create()
+        try:
+            with open('input', 'w') as f:
+                f.write("help help\n")
+
+            with open('output', 'wb') as outf, open('input', 'rb') as inf:
+
+                status = debugger.SetOutputFile(outf)
+                if status.Fail():
+                    raise Exception(status)
+
+                status = debugger.SetInputFile(inf)
+                if status.Fail():
+                    raise Exception(status)
+
+                opts = lldb.SBCommandInterpreterRunOptions()
+                debugger.RunCommandInterpreter(True, False, opts, 0, False, False)
+                debugger.GetOutputFile().Flush()
+
+            with open('output', 'r') as f:
+                self.assertTrue(re.search(r'Show a list of all debugger commands', f.read()))
+
+        finally:
+            self.RemoveTempFile('output')
+            self.RemoveTempFile('input')
+            lldb.SBDebugger.Destroy(debugger)
 
 
 
+    @add_test_categories(['pyapi'])
+    @no_debug_info_test
+    def test_fileno_error(self):
+
+        debugger = lldb.SBDebugger.Create()
+        try:
+            with open('output', 'w') as f:
+
+                sbf = lldb.SBFile()
+                sbf.SetDescriptor(f.fileno(), 'w', False)
+                status = debugger.SetErrorFile(sbf)
+                if status.Fail():
+                    raise Exception(status)
+
+                handle_command(debugger, 'lolwut', raise_on_fail=False, collect_result=False)
+
+            with open('output', 'r') as f:
+                errors = f.read()
+                self.assertTrue(re.search(r'error:.*lolwut', errors))
+
+        finally:
+            self.RemoveTempFile('output')
+            lldb.SBDebugger.Destroy(debugger)
 
 
+    @add_test_categories(['pyapi'])
+    @no_debug_info_test
+    def test_file_error(self):
+
+        debugger = lldb.SBDebugger.Create()
+        try:
+            with open('output', 'w') as f:
+
+                status = debugger.SetErrorFile(f)
+                if status.Fail():
+                    raise Exception(status)
+
+                handle_command(debugger, 'lolwut', raise_on_fail=False, collect_result=False)
+
+            with open('output', 'r') as f:
+                errors = f.read()
+                self.assertTrue(re.search(r'error:.*lolwut', errors))
+
+        finally:
+            self.RemoveTempFile('output')
+            lldb.SBDebugger.Destroy(debugger)
+
+
+    @add_test_categories(['pyapi'])
+    @no_debug_info_test
+    def test_legacy_file_error(self):
+
+        debugger = lldb.SBDebugger.Create()
+        try:
+            with open('output', 'w') as f:
+                debugger.SetErrorFileHandle(f, False)
+                handle_command(debugger, 'lolwut', raise_on_fail=False, collect_result=False)
+                debugger.GetErrorFileHandle().write('FOOBAR\n')
+
+            with open('output', 'r') as f:
+                errors = f.read()
+                self.assertTrue(re.search(r'error:.*lolwut', errors))
+                self.assertTrue(re.search(r'FOOBAR', errors))
+
+        finally:
+            self.RemoveTempFile('output')
+            lldb.SBDebugger.Destroy(debugger)
+
+
+    @add_test_categories(['pyapi'])
+    @no_debug_info_test
+    def test_string_out(self):
+        f = io.StringIO()
+        debugger = lldb.SBDebugger.Create()
+        try:
+            status = debugger.SetOutputFile(f)
+            if status.Fail():
+                raise Exception(status)
+            handle_command(debugger, "script 'foobar'")
+            self.assertEqual(f.getvalue().strip(), "'foobar'")
+        finally:
+            lldb.SBDebugger.Destroy(debugger)
+
+
+    @add_test_categories(['pyapi'])
+    @no_debug_info_test
+    def test_stdout(self):
+        f = io.StringIO()
+        debugger = lldb.SBDebugger.Create()
+        try:
+            status = debugger.SetOutputFile(f)
+            if status.Fail():
+                raise Exception(status)
+            handle_command(debugger, r"script sys.stdout.write('foobar\n')")
+            self.assertEqual(f.getvalue().strip(), "foobar\n7")
+        finally:
+            lldb.SBDebugger.Destroy(debugger)
+
+
+    @add_test_categories(['pyapi'])
+    @no_debug_info_test
+    def test_string_error(self):
+
+        f = io.StringIO()
+        debugger = lldb.SBDebugger.Create()
+        try:
+            status = debugger.SetErrorFile(f)
+            if status.Fail():
+                raise Exception
+            handle_command(debugger, 'lolwut', raise_on_fail=False, collect_result=False)
+
+            errors = f.getvalue()
+            self.assertTrue(re.search(r'error:.*lolwut', errors))
+
+        finally:
+            lldb.SBDebugger.Destroy(debugger)
+
+
+    @add_test_categories(['pyapi'])
+    @no_debug_info_test
+    def test_string_inout(self):
+
+        debugger = lldb.SBDebugger.Create()
+        try:
+
+            inf = io.StringIO("help help\nhelp b\n")
+            outf = io.StringIO()
+
+            status = debugger.SetOutputFile(outf)
+            if status.Fail():
+                raise Exception(status)
+
+            status = debugger.SetInputFile(inf)
+            if status.Fail():
+                raise Exception(status)
+
+            opts = lldb.SBCommandInterpreterRunOptions()
+            debugger.RunCommandInterpreter(True, False, opts, 0, False, False)
+            debugger.GetOutputFile().Flush()
+
+            self.assertTrue(re.search(r'Show a list of all debugger commands', outf.getvalue()))
+            self.assertTrue(re.search(r'Set a breakpoint', outf.getvalue()))
+
+        finally:
+            lldb.SBDebugger.Destroy(debugger)
+
+
+    @add_test_categories(['pyapi'])
+    @no_debug_info_test
+    def test_bytes_inout(self):
+
+        debugger = lldb.SBDebugger.Create()
+        try:
+
+            inf = io.BytesIO(b"help help\nhelp b\n")
+            outf = io.BytesIO()
+
+            status = debugger.SetOutputFile(outf)
+            if status.Fail():
+                raise Exception(status)
+
+            status = debugger.SetInputFile(inf)
+            if status.Fail():
+                raise Exception(status)
+
+            opts = lldb.SBCommandInterpreterRunOptions()
+            debugger.RunCommandInterpreter(True, False, opts, 0, False, False)
+            debugger.GetOutputFile().Flush()
+
+            self.assertTrue(re.search(b'Show a list of all debugger commands', outf.getvalue()))
+            self.assertTrue(re.search(b'Set a breakpoint', outf.getvalue()))
+
+        finally:
+            lldb.SBDebugger.Destroy(debugger)
+
+    @add_test_categories(['pyapi'])
+    @no_debug_info_test
+    def test_replace_stdout(self):
+        f = io.StringIO()
+        debugger = lldb.SBDebugger.Create()
+        try:
+            with replace_stdout(f):
+                self.assertEqual(sys.stdout, f)
+                handle_command(debugger, 'script sys.stdout.write("lol")',
+                    collect_result=False)
+                self.assertEqual(sys.stdout, f)
+        finally:
+            lldb.SBDebugger.Destroy(debugger)
+
+
+    @add_test_categories(['pyapi'])
+    @no_debug_info_test
+    def test_replace_stdout_with_nonfile(self):
+
+        f = io.StringIO()
+
+        with replace_stdout(f):
+
+            class Nothing():
+                pass
+
+            debugger = lldb.SBDebugger.Create()
+            try:
+                with replace_stdout(Nothing):
+                    self.assertEqual(sys.stdout, Nothing)
+                    handle_command(debugger, 'script sys.stdout.write("lol")',
+                        collect_result=False)
+                    self.assertEqual(sys.stdout, Nothing)
+            finally:
+                lldb.SBDebugger.Destroy(debugger)
+
+            sys.stdout.write(u"FOO")
+
+        self.assertEqual(f.getvalue(), "FOO")
+
+
+    @add_test_categories(['pyapi'])
+    @no_debug_info_test
+    def test_stream_error(self):
+
+        messages = list()
+
+        io = BadIO()
+        debugger = lldb.SBDebugger.Create()
+        try:
+            debugger.SetOutputFile(io)
+            debugger.SetLoggingCallback(messages.append)
+            handle_command(debugger, 'log enable lldb script')
+            handle_command(debugger, 'script 1')
+
+        finally:
+            lldb.SBDebugger.Destroy(debugger)
+
+        self.comment(messages)
+        self.assertTrue(any('OH NOE' in msg for msg in messages))
+
+
+    @add_test_categories(['pyapi'])
+    @no_debug_info_test
+    def test_identity(self):
+        f = io.StringIO()
+        sbf = lldb.SBFile()
+        sbf.SetFile(f)
+        self.assertTrue(f is sbf.GetFile())
+        sbf.Close()
+        self.assertTrue(f.closed)
+
+        f = io.StringIO()
+        sbf.SetFileBorrowed(f)
+        self.assertTrue(f is sbf.GetFile())
+        sbf.Close()
+        self.assertFalse(f.closed)
+
+        try:
+            with open('output', 'w') as f:
+                sbf.SetFile(f)
+                self.assertTrue(f is sbf.GetFile())
+                sbf.Close()
+                self.assertTrue(f.closed)
+
+            with open('output', 'w') as f:
+                sbf.SetFileBorrowed(f)
+                self.assertFalse(f is sbf.GetFile())
+                sbf.Write(b"foobar\n")
+                self.assertEqual(f.fileno(), sbf.GetFile().fileno())
+                sbf.Close()
+                self.assertFalse(f.closed)
+
+            with open('output', 'r') as f:
+                self.assertEqual("foobar", f.read().strip())
+
+            with open('output', 'wb') as f:
+                sbf.SetFileBorrowedForcingUseOfScriptingIOMethods(f)
+                self.assertTrue(f is sbf.GetFile())
+                sbf.Write(b"foobar\n")
+                self.assertEqual(f.fileno(), sbf.GetFile().fileno())
+                sbf.Close()
+                self.assertFalse(f.closed)
+
+            with open('output', 'r') as f:
+                self.assertEqual("foobar", f.read().strip())
+
+            with open('output', 'wb') as f:
+                sbf.SetFileForcingUseOfScriptingIOMethods(f)
+                self.assertTrue(f is sbf.GetFile())
+                sbf.Write(b"foobar\n")
+                self.assertEqual(f.fileno(), sbf.GetFile().fileno())
+                sbf.Close()
+                self.assertTrue(f.closed)
+
+            with open('output', 'r') as f:
+                self.assertEqual("foobar", f.read().strip())
+
+
+        finally:
+            self.RemoveTempFile('output')
