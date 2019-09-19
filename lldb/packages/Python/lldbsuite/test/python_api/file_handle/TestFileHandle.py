80673a5a653a78716e1c836c38bd3c8714f939c7
Lawrence D'Anna <lawrence_danna@apple.com>
2019-09-19 12:06:03 -0700

    SBFile::GetFile: convert SBFile back into python native files.
    This makes SBFile::GetFile public and adds a SWIG typemap to convert
    the result back into a python native file.
    
    If the underlying File itself came from a python file, it is returned
    identically.   Otherwise a new python file object is created using
    the file descriptor.

--- lldb/packages/Python/lldbsuite/test/python_api/file_handle/TestFileHandle.py
+++ lldb/packages/Python/lldbsuite/test/python_api/file_handle/TestFileHandle.py
@@ -773,7 +773,6 @@ class FileHandleTestCase(lldbtest.TestBase):
 
 
     @add_test_categories(['pyapi'])
-    @skipIf(True) # FIXME implement SBFile::GetFile
     @skipIf(py_version=['<', (3,)])
     def test_identity(self):
 
