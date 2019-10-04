27004277e8e39847acf8eb290b9fcc2d4502e454
Lawrence D'Anna <lawrence_danna@apple.com>
2019-10-03 21:52:58 -0700

    SBFile support in SBCommandReturnObject
    Summary:
    This patch add SBFile interfaces to SBCommandReturnObject, and
    removes the internal callers of its FILE* interfaces.
    
    Reviewers: JDevlieghere, jasonmolenda, labath
    
    Subscribers: lldb-commits
    
    Tags: #lldb
    
    Differential Revision: https://reviews.llvm.org/D68434

--- lldb/packages/Python/lldbsuite/test/python_api/file_handle/TestFileHandle.py
+++ lldb/packages/Python/lldbsuite/test/python_api/file_handle/TestFileHandle.py
@@ -310,7 +310,7 @@ class FileHandleTestCase(lldbtest.TestBase):
 
 
     @add_test_categories(['pyapi'])
-    @expectedFailure # FIXME need SBFile interfaces on SBCommandReturnObject
+    @skipIf(py_version=['<', (3,)])
     def test_immediate_string(self):
         f = io.StringIO()
         ret = lldb.SBCommandReturnObject()
@@ -326,7 +326,7 @@ class FileHandleTestCase(lldbtest.TestBase):
 
 
     @add_test_categories(['pyapi'])
-    @expectedFailure # FIXME need SBFile interfaces on SBCommandReturnObject
+    @skipIf(py_version=['<', (3,)])
     def test_immediate_sbfile_string(self):
         f = io.StringIO()
         ret = lldb.SBCommandReturnObject()
