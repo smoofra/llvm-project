364a0e85c6380d6ad7aa6f797e01587a647ad853
Lawrence D'Anna <lawrence_danna@apple.com>
2019-10-07 15:38:19 -0700

    SBFile::GetFile: convert SBFile back into python native files.
    This makes SBFile::GetFile public and adds a SWIG typemap to convert
    the result back into a python native file.
    
    If the underlying File itself came from a python file, it is returned
    identically.   Otherwise a new python file object is created using
    the file descriptor.

--- lldb/source/Plugins/ScriptInterpreter/Python/PythonDataObjects.cpp
+++ lldb/source/Plugins/ScriptInterpreter/Python/PythonDataObjects.cpp
@@ -1008,8 +1008,6 @@ operator()(std::initializer_list<PythonObject> args) {
 
 PythonFile::PythonFile() : PythonObject() {}
 
-PythonFile::PythonFile(File &file, const char *mode) { Reset(file, mode); }
-
 PythonFile::PythonFile(PyRefType type, PyObject *o) { Reset(type, o); }
 
 PythonFile::~PythonFile() {}
@@ -1059,22 +1057,35 @@ void PythonFile::Reset(PyRefType type, PyObject *py_obj) {
   PythonObject::Reset(PyRefType::Borrowed, result.get());
 }
 
-void PythonFile::Reset(File &file, const char *mode) {
-  if (!file.IsValid()) {
-    Reset();
-    return;
-  }
+Expected<PythonFile> PythonFile::FromFile(File &file, const char *mode) {
+  if (!file.IsValid())
+      return llvm::createStringError(llvm::inconvertibleErrorCode(),
+                                   "invalid file");
 
-  char *cmode = const_cast<char *>(mode);
+  PyObject *file_obj = (PyObject *)file.GetPythonObject();
+  if (file_obj)
+    return Retain<PythonFile>(file_obj);
+
+  if (!mode)
+    mode = file.GetOpenMode();
+  if (!mode)
+      return llvm::createStringError(llvm::inconvertibleErrorCode(),
+                                   "can't determine open mode for file");
+
 #if PY_MAJOR_VERSION >= 3
-  Reset(PyRefType::Owned, PyFile_FromFd(file.GetDescriptor(), nullptr, cmode,
-                                        -1, nullptr, "ignore", nullptr, 0));
+  file_obj = PyFile_FromFd(file.GetDescriptor(), nullptr, mode,
+                                        -1, nullptr, "ignore", nullptr, 0);
 #else
   // Read through the Python source, doesn't seem to modify these strings
-  Reset(PyRefType::Owned,
-        PyFile_FromFile(file.GetStream(), const_cast<char *>(""), cmode,
-                        nullptr));
+  char *cmode = const_cast<char *>(mode);
+  file_obj = PyFile_FromFile(file.GetStream(), const_cast<char *>(""), cmode,
+                        nullptr);
 #endif
+
+  if (!file_obj)
+    return exception();
+
+  return Take<PythonFile>(file_obj);
 }
 
 
@@ -1230,6 +1241,8 @@ public:
       return py_error;
     return base_error;
   };
+
+  void *GetPythonObject() const override { return m_py_obj.get(); }
 
 protected:
   PythonFile m_py_obj;
